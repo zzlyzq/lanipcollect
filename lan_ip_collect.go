80bb8b537152ba05696e5b2ca38d949bd367a3f0
jacky <18910644801@189.cn>
2024-06-27 12:57:20 +0800

    修正错误，实现对收取的ARP数据包进行分析，以及打印debug信息，用于更好的运行时观测状态。

--- lan_ip_collect.go
+++ lan_ip_collect.go
@@ -22,7 +22,7 @@ import (
 
 var (
     snapshotLen int32         = 1024
-    promiscuous bool          = false
+    promiscuous bool          = true
     err         error
     timeout     time.Duration = 30 * time.Second
     handle      *pcap.Handle
@@ -36,33 +36,25 @@ var (
 )
 
 func main() {
-    // 创建日志文件
     logFile, err := os.OpenFile("log.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
     if err != nil {
         log.Fatal(err)
     }
     defer logFile.Close()
 
-    // 创建一个新的日志记录器
     logger = log.New(io.MultiWriter(os.Stdout, logFile), "", log.LstdFlags)
-
-    // 设置输出编码为UTF-8
     logger.SetPrefix("\xEF\xBB\xBF")
 
-    // 获取所有网络接口
     devices, err := pcap.FindAllDevs()
     if err != nil {
         logger.Fatal(err)
     }
 
-    // 打印网卡信息
     fmt.Println("Available network interfaces:")
     for i, device := range devices {
         fmt.Printf("%d. %s\n", i+1, device.Name)
         fmt.Printf("   Description: %s\n", device.Description)
         fmt.Printf("   Flags: %s\n", device.Flags)
-
-        // 打印网卡的IP地址信息
         fmt.Printf("   IP Addresses:\n")
         for _, addr := range device.Addresses {
             if addr.IP.To4() != nil {
@@ -70,7 +62,6 @@ func main() {
             }
         }
 
-        // 检查网卡是否已经插入线缆
         isConnected := false
         for _, addr := range device.Addresses {
             if addr.IP.IsGlobalUnicast() {
@@ -87,23 +78,19 @@ func main() {
         fmt.Println()
     }
 
-    // 提示用户选择网卡
     fmt.Print("Enter the number of the interface to listen on: ")
     reader := bufio.NewReader(os.Stdin)
     input, _ := reader.ReadString('\n')
     input = strings.TrimSpace(input)
 
-    // 解析用户输入的序号
     index, err := strconv.Atoi(input)
     if err != nil || index < 1 || index > len(devices) {
         logger.Fatal("Invalid interface number")
     }
 
-    // 选择网卡
     device := devices[index-1]
     logger.Print("开始抓包：\r\n")
 
-    // 打开网络接口
     handle, err = pcap.OpenLive(device.Name, snapshotLen, promiscuous, timeout)
     if err != nil {
         logger.Fatal(err)
@@ -112,49 +99,49 @@ func main() {
 
     packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
     for packet := range packetSource.Packets() {
+        logger.Printf("Captured packet: %v\n", packet)
         arpLayer := packet.Layer(layers.LayerTypeARP)
         if arpLayer != nil {
             arpPacket := arpLayer.(*layers.ARP)
-            if arpPacket.Operation == layers.ARPReply {
-                ip := net.IP(arpPacket.SourceProtAddress).String()
-                mac := net.HardwareAddr(arpPacket.SourceHwAddress).String()
+            logger.Printf("ARP packet: %v\n", arpPacket)
+            ip := net.IP(arpPacket.SourceProtAddress).String()
+            mac := net.HardwareAddr(arpPacket.SourceHwAddress).String()
+            logger.Printf("Captured ARP packet: IP %s - MAC %s\n", ip, mac)
 
-                if _, ok := ipMap[ip]; !ok {
-                    ipMap[ip] = mac
-                }
-
-                if _, ok := macMap[mac]; !ok {
-                    macMap[mac] = []string{}
-                    macVendorMap[mac] = getVendor(mac)
-                }
-
-                if !contains(macMap[mac], ip) {
-                    macMap[mac] = append(macMap[mac], ip)
-                }
-
-                // 包含厂商信息的结构
-                jsonData := make(map[string]interface{})
-                for mac, ips := range macMap {
-                    jsonData[mac] = map[string]interface{}{
-                        "ips":    ips,
-                        "vendor": macVendorMap[mac],
-                    }
-                }
-
-                data, err := json.MarshalIndent(jsonData, "", "  ")
-                if err != nil {
-                    logger.Printf("Error marshalling JSON: %s\n", err)
-                    continue
-                }
-
-                err = os.WriteFile("lanipcollect.json", data, 0644)
-                if err != nil {
-                    logger.Printf("Error writing JSON file: %s\n", err)
-                }
-
-                logger.Printf("Updated mapping: MAC %s (%s) - IP %s\n", mac, macVendorMap[mac], ip)
-                writeToExcel(macMap, macVendorMap)
+            if _, ok := ipMap[ip]; !ok {
+                ipMap[ip] = mac
             }
+
+            if _, ok := macMap[mac]; !ok {
+                macMap[mac] = []string{}
+                macVendorMap[mac] = getVendor(mac)
+            }
+
+            if !contains(macMap[mac], ip) {
+                macMap[mac] = append(macMap[mac], ip)
+            }
+
+            jsonData := make(map[string]interface{})
+            for mac, ips := range macMap {
+                jsonData[mac] = map[string]interface{}{
+                    "ips":    ips,
+                    "vendor": macVendorMap[mac],
+                }
+            }
+
+            data, err := json.MarshalIndent(jsonData, "", "  ")
+            if err != nil {
+                logger.Printf("Error marshalling JSON: %s\n", err)
+                continue
+            }
+
+            err = os.WriteFile("lanipcollect.json", data, 0644)
+            if err != nil {
+                logger.Printf("Error writing JSON file: %s\n", err)
+            }
+
+            logger.Printf("Updated mapping: MAC %s (%s) - IP %s\n", mac, macVendorMap[mac], ip)
+            writeToExcel(macMap, macVendorMap)
         }
     }
 }
@@ -180,7 +167,7 @@ func getVendor(mac string) string {
         }
 
         file, err := os.Open("oui.txt")
-        if (err != nil) {
+        if err != nil {
             fmt.Println("Error opening file:", err)
             os.Exit(1)
         }
@@ -212,13 +199,11 @@ func writeToExcel(macMap map[string][]string, macVendorMap map[string]string) {
     f := excelize.NewFile()
     sheet := "Sheet1"
 
-    // 设置表头
     f.SetCellValue(sheet, "A1", "序号")
     f.SetCellValue(sheet, "B1", "MAC地址")
     f.SetCellValue(sheet, "C1", "IP地址")
     f.SetCellValue(sheet, "D1", "厂商")
 
-    // 收集和排序数据
     type MacEntry struct {
         Mac     string
         Ips     []string
@@ -227,7 +212,6 @@ func writeToExcel(macMap map[string][]string, macVendorMap map[string]string) {
 
     var entries []MacEntry
     for mac, ips := range macMap {
-        // 按IP地址的所有八位组依次排序
         sort.Slice(ips, func(i, j int) bool {
             return compareIPs(ips[i], ips[j])
         })
@@ -238,21 +222,19 @@ func writeToExcel(macMap map[string][]string, macVendorMap map[string]string) {
         })
     }
 
-    // 按第一个IP地址的所有八位组依次排序
     sort.Slice(entries, func(i, j int) bool {
         return compareIPs(entries[i].Ips[0], entries[j].Ips[0])
     })
 
-    // 写入数据
     row := 2
-    for i, entry := range entries {
-        for _, ip := range entry.Ips {
-            f.SetCellValue(sheet, fmt.Sprintf("A%d", row), i+1)
-            f.SetCellValue(sheet, fmt.Sprintf("B%d", row), entry.Mac)
-            f.SetCellValue(sheet, fmt.Sprintf("C%d", row), ip)
-            f.SetCellValue(sheet, fmt.Sprintf("D%d", row), entry.Vendor)
-            row++
-        }
+    serial := 1
+    for _, entry := range entries {
+        f.SetCellValue(sheet, fmt.Sprintf("A%d", row), serial)
+        f.SetCellValue(sheet, fmt.Sprintf("B%d", row), entry.Mac)
+        f.SetCellValue(sheet, fmt.Sprintf("C%d", row), strings.Join(entry.Ips, "\n"))
+        f.SetCellValue(sheet, fmt.Sprintf("D%d", row), entry.Vendor)
+        row++
+        serial++
     }
 
     if err := f.SaveAs("lanipcollect.xlsx"); err != nil {
